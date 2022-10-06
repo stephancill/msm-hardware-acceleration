0cf61d99866cfeae8add2d136cc73019e19e127f
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2022-10-06 03:40:31 +0200

    Montgomery reduction

--- sim/test_ff.py
+++ sim/test_ff.py
@@ -1,8 +1,15 @@
 from field_arithmetic.barrett import BarrettReduction
+from field_arithmetic.montgomery import MontgomeryReduction
 
 
 def test_barrett_reduction():
     field = BarrettReduction(65521, 16)
+    a = 64111
+    b = 11195
+    assert field.ff_mul(a, b) == a * b % field.s
+
+def test_montgomery_reduction():
+    field = MontgomeryReduction(65521)
     a = 64111
     b = 11195
     assert field.ff_mul(a, b) == a * b % field.s
